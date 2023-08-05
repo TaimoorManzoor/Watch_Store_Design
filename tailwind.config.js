0a2e6ec02260cf2c0f2f1ada4beaa878ec5a8a95
Taimoor Manzoor <taimoormanzoor2001@gmail.com>
2023-08-05 16:09:09 +0500

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*"],
+  theme: {
+    screens:{
+      sm:"380px",
+      md:"680px",
+      lg:"976px",
+      xl:"1440px",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
